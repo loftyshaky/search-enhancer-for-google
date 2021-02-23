6b316e3f5aff1f7f459de22cf344c79ed0e6534d
loftyshaky <loftyshaky@gmail.com>
2021-02-23 13:18:24 +1000

    feat: implement indexedDB

--- src/ts/background/init.ts
+++ src/ts/background/init.ts
@@ -1,5 +1,9 @@
-import { data } from 'background/internal';
+import {
+    data,
+    db,
+} from 'background/internal';
 
 export const init = (): void => {
     data.Main.i().set_from_storage();
+    db.Main.i().init_db();
 };
--- src/ts/background/internal.ts
+++ src/ts/background/internal.ts
@@ -4,5 +4,6 @@ misplaced_dependency('background');
 
 export * as data from 'background/data';
 export * as icons from 'background/icons';
+export * as db from 'background/db';
 
 export * from 'background/init';
