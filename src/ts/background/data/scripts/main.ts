13327fe4fd472b6ef529a14ecad791cb3c52f54d
loftyshaky <loftyshaky@gmail.com>
2021-04-15 06:20:28 +1000

    style: format

--- src/ts/background/data/scripts/main.ts
+++ src/ts/background/data/scripts/main.ts
@@ -6,12 +6,12 @@ export class Main {
     private static i0: Main;
 
     public static i(): Main {
-    // eslint-disable-next-line no-return-assign
+        // eslint-disable-next-line no-return-assign
         return this.i0 || (this.i0 = new this());
     }
 
     // eslint-disable-next-line no-useless-constructor, @typescript-eslint/no-empty-function
-    private constructor() {}
+    private constructor() { }
 
     public defaults: any = {
         side_panel_position: 'left',
