68fdfb1674d7d1e87394d45a2c71cb620c9e6294
loftyshaky <loftyshaky@gmail.com>
2022-02-19 01:49:45 +1000

    feat: inject different content scripts depending on env

--- src/ts/background/init.ts
+++ src/ts/background/init.ts
@@ -1,3 +1,4 @@
+import { s_env } from 'shared/internal';
 import { s_data, s_icons } from 'background/internal';
 
 export const init = (): Promise<void> =>
@@ -6,13 +7,20 @@ export const init = (): Promise<void> =>
         await s_data.Main.i().set_from_storage({ transform: true });
         await s_icons.Main.i().generate_ip_to_country_arr();
 
-        ext.inject_js_and_css_in_content_script(
-            [
-                'content_script.js',
-                'chunks/vendors-node_modules_mobx-react_dist_mobxreact_esm_js.js',
-                'chunks/vendors-node_modules_mobx-utils_mobx-utils_module_js-node_modules_tinycolor2_tinycolor_js.js',
-                'chunks/src_ts_content_script_internal_ts.js',
-            ],
-            ['content_script_css.css'],
-        );
+        if (s_env.Main.i().is_dev()) {
+            ext.inject_js_and_css_in_content_script(
+                [
+                    'content_script.js',
+                    'chunks/vendors-node_modules_mobx-react_dist_mobxreact_esm_js.js',
+                    'chunks/vendors-node_modules_mobx-utils_mobx-utils_module_js-node_modules_tinycolor2_tinycolor_js.js',
+                    'chunks/src_ts_content_script_internal_ts.js',
+                ],
+                ['content_script_css.css'],
+            );
+        } else {
+            ext.inject_js_and_css_in_content_script(
+                ['content_script.js'],
+                ['content_script_css.css'],
+            );
+        }
     }, 'ges_1016');
