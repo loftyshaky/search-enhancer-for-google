9bfaf97cfe7fd110e918bb05bead367f1097ecc3
loftyshaky <loftyshaky@gmail.com>
2022-02-18 22:34:16 +1000

    fix: search results appending under footer in All

--- src/ts/content_script/mutations/scripts/observer.ts
+++ src/ts/content_script/mutations/scripts/observer.ts
@@ -1,38 +1,43 @@
 import { s_suffix } from 'shared/internal';
-import { s_actions, s_infinite_scroll } from 'content_script/internal';
+import { s_actions, s_infinite_scroll, s_location } from 'content_script/internal';
 
 const observer = new MutationObserver((mutations): void =>
     err(() => {
-        mutations.forEach((mutation): void =>
-            err(() => {
-                if (
-                    !x.matches(
-                        mutation.target as HTMLElement,
-                        `.${new s_suffix.Main('root_parent').result}`,
-                    ) &&
-                    !x.matches(
-                        mutation.target as HTMLElement,
-                        `.${new s_suffix.Main('icons').result}`,
-                    ) &&
-                    n(mutation.addedNodes[0]) &&
-                    !x.matches(
-                        mutation.addedNodes[0] as HTMLElement,
-                        `.${new s_suffix.Main('icons').result}`,
-                    ) &&
-                    !x.matches(
-                        mutation.addedNodes[0] as HTMLElement,
-                        `.${new s_suffix.Main('img_action_bar').result}`,
-                    ) &&
-                    !x.matches(
-                        mutation.addedNodes[0] as HTMLElement,
-                        `.${new s_suffix.Main('spinner').result}`,
-                    )
-                ) {
-                    s_actions.Main.i().run_reload_actions_2_debounce();
+        mutations.forEach(
+            (mutation): Promise<void> =>
+                err_async(async () => {
+                    if (
+                        !x.matches(
+                            mutation.target as HTMLElement,
+                            `.${new s_suffix.Main('root_parent').result}`,
+                        ) &&
+                        !x.matches(
+                            mutation.target as HTMLElement,
+                            `.${new s_suffix.Main('icons').result}`,
+                        ) &&
+                        n(mutation.addedNodes[0]) &&
+                        !x.matches(
+                            mutation.addedNodes[0] as HTMLElement,
+                            `.${new s_suffix.Main('icons').result}`,
+                        ) &&
+                        !x.matches(
+                            mutation.addedNodes[0] as HTMLElement,
+                            `.${new s_suffix.Main('img_action_bar').result}`,
+                        ) &&
+                        !x.matches(
+                            mutation.addedNodes[0] as HTMLElement,
+                            `.${new s_suffix.Main('spinner').result}`,
+                        )
+                    ) {
+                        s_actions.Main.i().run_reload_actions_2_debounce();
 
-                    s_infinite_scroll.FooterEls.i().append_to_footer();
-                }
-            }, 'ges_1087'),
+                        if (s_location.Main.i().is_shopping_page) {
+                            await x.delay(1000);
+
+                            s_infinite_scroll.FooterEls.i().append_to_footer();
+                        }
+                    }
+                }, 'ges_1087'),
         );
     }, 'ges_1088'),
 );
